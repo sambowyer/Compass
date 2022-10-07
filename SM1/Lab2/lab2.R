d3f1943b132238e79c56909cd4c463bc4c283fd7
sambowyer <sambowyer2@gmail.com>
2022-10-07 11:07:20 +0100

    Finished SM1 lab2 (for now)

--- SM1/Lab2/lab2.R
+++ SM1/Lab2/lab2.R
@@ -1,7 +1,7 @@
 
 # Generate the data
 n = 200
-xs = seq(0, 2.5, length.out=n)
+xs = seq(-2.5, 2.5, length.out=n)
 ys = exp(1.5*xs - 1) + rnorm(n, 0, 0.64)
 
 # Choose default hyperparameters (max degree of feature transform, deg, and regularization coefficient, lambda)
@@ -17,8 +17,8 @@ lambda = 4
 #'
 #' @return transformed data x
 phi <- function(x, d=deg){
-  phiX = matrix(NA, nrow=n, ncol=m)
-  for(i in 1:n){
+  phiX = matrix(NA, nrow=length(x), ncol=d+1)
+  for(i in 1:length(x)){
     row = rep(x[i], d+1)
     for (j in 0:d){
       row[j+1] = row[j+1]**j
@@ -43,10 +43,10 @@ fit <- function(X, y, regcoeff=lambda){
 #' Calculate the predicted output value for a single input x using weights w
 #'
 #' @param x input value
-#' @value w weights
+#' @param w weights
 #'
 #' @return predicted output
-predict <- function(x){
+predict <- function(x, w){
   y = 0
   for (i in 0:deg){
     y = y + w[i+1] * (x**i)
@@ -54,26 +54,99 @@ predict <- function(x){
   return(y)
 }
 
+#' Compute mean CV error (leave-one-out CV)
+#'
+#' @param xs input values
+#' @param ys target values
+#' @param deg maximum degree of feature transform
+#' @param lambda regularization coefficient
+#'
+#' @return mean CV error
 crossValidation <- function(xs=xs, ys=ys, deg=deg, lambda=lambda){
-  # TODO: Fill this in and use it to find an optimal lambda (for a given deg)
+  err = 0
+  for (i in 1:length(xs)){
+    phiX = phi(xs[-i], deg)
+    w = fit(phiX, ys[-i], lambda)
+    predYs = phiX %*% w
+    err = err + norm(ys[-i]-predYs)
+  }
+  return(err/length(xs))
 }
+
+findOptimumDegAndLambda <- function(xs, ys, dValues=2:7, lambdaVals=c(0, 0.005, 0.01, 0.1, 0.5, 1, 2, 5, 10, 25), verbose=TRUE){
+  optErr = Inf
+  optLambda = NA
+  optD = NA
+  
+  if(verbose){
+    print("deg      lambda  err")
+  }
+  
+  for(d in dValues){
+    for(lambda in lambdaVals){
+      err = crossValidation(xs, ys, deg, lambda)
+      if(verbose){
+        # sprintf("lambda=%.4f, err=%.4f", lambda, err)
+        print(c(d, lambda, err))
+      }
+      if (err < optErr){
+        optErr = err
+        optLambda = lambda
+        optD = d
+      }
+    }
+  }
+  return(c(d, optLambda))
+}
+# phiX = phi(xs, deg)
+# w = fit(phiX, ys, lambda)
+
+
+plotVsTrue <- function(xs, ys, w, sd=NA){
+  predictGivenW <- function(x){
+    return(predict(x, w))
+  }
+  
+  predictGivenWPlusSd <- function(x){
+    return(predict(x, w) + sd)
+  }
+  predictGivenWMinusSd <- function(x){
+    return(predict(x, w) - sd)
+  }
+  
+  plot(xs, ys, xlab="x", ylab="y")
+  curve(exp(1.5*x -1), add=TRUE, lty=2, lwd=4)
+  curve(predictGivenW, add=TRUE, col="red", lwd=2)
+  # curve(phi(x)%*%w, add=TRUE, col="red", lwd=2)
+  
+  # Add standard deviation 'tube'
+  if (!is.na(sd)){
+    curve(predictGivenWPlusSd, add=TRUE, col="red", lty=2, lwd=2)
+    curve(predictGivenWMinusSd, add=TRUE, col="red", lty=2, lwd=2)
+  }
+  
+  legend("topleft", legend=c("True", "Prediction"),
+         col=c("black", "red"), lty=c(2,1), cex=0.8)
+  # lines(xs, phiX%*%w, col="red", lwd=2)
+}
+
+params = findOptimumDegAndLambda(xs, ys, verbose=FALSE)
+deg    = params[1]
+lambda = params[2]
+print(sprintf("optimum d=%s, lambda=%s", deg, lambda))
 
 phiX = phi(xs, deg)
-
 w = fit(phiX, ys, lambda)
+predYs = phiX %*% w
+variance = norm(ys - predYs)/(n-1)
 
-plotVsTrue <- function(xs, ys, w){
-  # TODO: Organize the plotting stuff into this function
-  #       And find a way to get the 'tube' of standard deviation
+# Find percentage of data points within 1 SD of prediction
+count = 0
+for (i in 1:length(xs)){
+  if(abs(predict(xs[i], w) - ys[i]) < sqrt(variance)){
+    count = count + 1
+  }
 }
+print(sprintf("Total percentage of data points within the 1SD 'tube': %s", 100*count/length(xs)))
 
-plot(xs, ys, xlab="x", ylab="y")
-
-
-curve(exp(1.5*x -1), add=TRUE, lty=2, lwd=4)
-curve(predict, add=TRUE, col="red", lwd=2)
-# curve(phi(x)%*%w, add=TRUE, col="red", lwd=2)
-
-legend("topleft", legend=c("True", "Prediction"),
-       col=c("black", "red"), lty=c(2,1), cex=0.8)
-# lines(xs, phiX%*%w, col="red", lwd=2)
+plotVsTrue(xs, ys, w, sqrt(variance))
