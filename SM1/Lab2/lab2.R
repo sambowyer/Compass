4099b7035f12fc238204dd5e09dda2c3b76cee76
sambowyer <sambowyer2@gmail.com>
2022-10-07 17:27:28 +0100

    mahoosive improvements to SM lab2

--- SM1/Lab2/lab2.R
+++ SM1/Lab2/lab2.R
@@ -1,13 +1,13 @@
 
 # Generate the data
 n = 200
-xs = seq(-2.5, 2.5, length.out=n)
+xs = seq(-1,3, length.out=n)
 ys = exp(1.5*xs - 1) + rnorm(n, 0, 0.64)
 
-# Choose default hyperparameters (max degree of feature transform, deg, and regularization coefficient, lambda)
-deg    = 7
-m      = deg+1
-lambda = 4
+# # Choose default hyperparameters (max degree of feature transform, deg, and regularization coefficient, lambda)
+# deg    = 7
+# m      = deg+1
+# lambda = 4
 
 
 #' Perform feature transformation of input data x
@@ -48,7 +48,7 @@ fit <- function(X, y, regcoeff=lambda){
 #' @return predicted output
 predict <- function(x, w){
   y = 0
-  for (i in 0:deg){
+  for (i in 0:length(w)-1){
     y = y + w[i+1] * (x**i)
   }
   return(y)
@@ -73,7 +73,7 @@ crossValidation <- function(xs=xs, ys=ys, deg=deg, lambda=lambda){
   return(err/length(xs))
 }
 
-findOptimumDegAndLambda <- function(xs, ys, dValues=2:7, lambdaVals=c(0, 0.005, 0.01, 0.1, 0.5, 1, 2, 5, 10, 25), verbose=TRUE){
+findOptimumDegAndLambda <- function(xs, ys, dValues=1:8, lambdaVals=0:100, verbose=TRUE){
   optErr = Inf
   optLambda = NA
   optD = NA
@@ -83,8 +83,14 @@ findOptimumDegAndLambda <- function(xs, ys, dValues=2:7, lambdaVals=c(0, 0.005, 
   }
   
   for(d in dValues){
+    errs = c()
+    
+    optErrTemp = Inf
+    optLambdaTemp = NA
+    
     for(lambda in lambdaVals){
-      err = crossValidation(xs, ys, deg, lambda)
+      err = crossValidation(xs, ys, d, lambda)
+      errs = c(errs, err)
       if(verbose){
         # sprintf("lambda=%.4f, err=%.4f", lambda, err)
         print(c(d, lambda, err))
@@ -94,7 +100,30 @@ findOptimumDegAndLambda <- function(xs, ys, dValues=2:7, lambdaVals=c(0, 0.005, 
         optLambda = lambda
         optD = d
       }
+      if (err < optErrTemp){
+        optErrTemp = err
+        optLambdaTemp= lambda
+      }
     }
+    par(mfrow=c(2,2))
+    plot(lambdaVals, errs, main=sprintf("d=%s", d))
+    
+    for (l in c(optLambdaTemp, lambdaVals[1], lambdaVals[length(lambdaVals)])){
+      wForNow = fit(phi(xs, d), ys, l)
+      
+      
+      predictGivenW <- function(x){
+        return(phi(x, d) %*% wForNow)
+      }
+      
+      plot(xs, ys, xlab="x", ylab="y", main=sprintf("d=%s, lambda=%s", d, l))#
+      curve(predictGivenW, add=TRUE, col="red", lty=2, lwd=3)
+      curve(exp(1.5*x -1), add=TRUE, lty=2, lwd=2)
+      
+    }
+    
+    # plotVsTrue(xs, ys, fit(phi(xs, d), ys, 68))
+    my.name <- readline(prompt="next?")
   }
   return(c(d, optLambda))
 }
@@ -102,19 +131,21 @@ findOptimumDegAndLambda <- function(xs, ys, dValues=2:7, lambdaVals=c(0, 0.005, 
 # w = fit(phiX, ys, lambda)
 
 
-plotVsTrue <- function(xs, ys, w, sd=NA){
+plotVsTrue <- function(xs, ys, w, sd=NA, title=""){
+  par(mfrow=c(1,1))
+  d = length(w)-1
   predictGivenW <- function(x){
-    return(predict(x, w))
+    return(phi(x, d) %*% w)
   }
   
   predictGivenWPlusSd <- function(x){
-    return(predict(x, w) + sd)
+    return((phi(x, d) %*% w) + sd)
   }
   predictGivenWMinusSd <- function(x){
-    return(predict(x, w) - sd)
+    return((phi(x, d) %*% w) - sd)
   }
   
-  plot(xs, ys, xlab="x", ylab="y")
+  plot(xs, ys, xlab="x", ylab="y", main=title)
   curve(exp(1.5*x -1), add=TRUE, lty=2, lwd=4)
   curve(predictGivenW, add=TRUE, col="red", lwd=2)
   # curve(phi(x)%*%w, add=TRUE, col="red", lwd=2)
@@ -130,10 +161,11 @@ plotVsTrue <- function(xs, ys, w, sd=NA){
   # lines(xs, phiX%*%w, col="red", lwd=2)
 }
 
+# Find optimal parameters
 params = findOptimumDegAndLambda(xs, ys, verbose=FALSE)
 deg    = params[1]
 lambda = params[2]
-print(sprintf("optimum d=%s, lambda=%s", deg, lambda))
+# print(sprintf("optimum d=%s, lambda=%s", deg, lambda))
 
 phiX = phi(xs, deg)
 w = fit(phiX, ys, lambda)
@@ -141,12 +173,8 @@ predYs = phiX %*% w
 variance = norm(ys - predYs)/(n-1)
 
 # Find percentage of data points within 1 SD of prediction
-count = 0
-for (i in 1:length(xs)){
-  if(abs(predict(xs[i], w) - ys[i]) < sqrt(variance)){
-    count = count + 1
-  }
-}
+diffs = abs(predYs - ys) - sqrt(variance)
+count = length(diffs[diffs < 0])
 print(sprintf("Total percentage of data points within the 1SD 'tube': %s", 100*count/length(xs)))
 
-plotVsTrue(xs, ys, w, sqrt(variance))
+plotVsTrue(xs, ys, w, sqrt(variance), title=sprintf("optimum d=%s, lambda=%s", deg, lambda))
